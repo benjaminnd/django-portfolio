c2fb41f5b1265dd773932251246e9a8b0387fc94
Minh <minhnhatdang1111@gmail.com>
2020-09-29 16:47:45 -0400

    Adding styles for project details page

--- jobs/models.py
+++ jobs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     name = models.CharField(max_length=200, default="")
-    github_url = models.CharField(max_length=200, default="")
+    github_url = models.CharField(max_length=200, default="", blank=True)
     demo_url = models.CharField(max_length=200, default="", blank=True)
     admin_credentials = models.CharField(max_length=200, default="", blank=True)
     summary = models.CharField(max_length=2000, blank=True)
