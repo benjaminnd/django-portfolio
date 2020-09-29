251cc6e46126203242b22d133e59c94e01de3938
Minh <minhnhatdang1111@gmail.com>
2020-09-29 12:35:54 -0400

    Initial project upload

---
+++ jobs/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=200, default="")
+    github_url = models.CharField(max_length=200, default="")
+    demo_url = models.CharField(max_length=200, default="", blank=True)
+    admin_credentials = models.CharField(max_length=200, default="", blank=True)
+    summary = models.CharField(max_length=2000, blank=True)
+    is_personal = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.summary
